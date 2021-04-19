5fc526b7514d38f02bebbcaf652368898b90f61a
lczm <zemingchua@gmail.com>
2021-04-19 16:53:36 +0800

    Working constant addition

--- main.go
+++ main.go
@@ -20,6 +20,15 @@ func readImage(filePath string) (image.Image, error) {
 	return image, err
 }
 
+// Given a uint16 (not a uint8) such that it does not overload,
+// clamp it back into a uint8, mainly used to add constants to colors
+func clamp(x uint16) uint8 {
+	if x >= 255 {
+		return 255
+	}
+	return uint8(x)
+}
+
 func main() {
 	fmt.Println("Hello, world.")
 	img, err := readImage("./wallpaper.jpg")
@@ -33,6 +42,7 @@ func main() {
 	point := img.Bounds().Size()
 	width := point.X
 	height := point.Y
+	var constant uint16 = 255
 
 	fmt.Println(fmt.Sprintf("Width : %d, Height : %d", width, height))
 	// Iterate every pixel
@@ -40,9 +50,9 @@ func main() {
 		for j := 0; j < height; j++ {
 			col := img.At(i, j)
 			r, g, b, _ := col.RGBA()
-			r8 := uint8(r / 256)
-			g8 := uint8(g / 256)
-			b8 := uint8(b / 256)
+			r8 := clamp(uint16(r/256) + constant)
+			g8 := clamp(uint16(g/256) + constant)
+			b8 := clamp(uint16(b/256) + constant)
 			mod.Set(i, j, color.RGBA{
 				R: r8,
 				G: g8,
