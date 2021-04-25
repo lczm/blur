b867b9d7fc9a3d393f6e2da9f72ff490244aa799
lczm <zemingchua@gmail.com>
2021-04-25 18:05:13 +0800

    Implement simple convolution blur

--- main.go
+++ main.go
@@ -22,11 +22,25 @@ func readImage(filePath string) (image.Image, error) {
 
 // Given a uint16 (not a uint8) such that it does not overload,
 // clamp it back into a uint8, mainly used to add constants to colors
-func clamp(x uint16) uint8 {
+func clampRGB(x uint16) uint8 {
 	if x >= 255 {
 		return 255
 	}
 	return uint8(x)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func main() {
@@ -36,23 +50,45 @@ func main() {
 		panic(err)
 	}
 	mod := image.NewRGBA(img.Bounds())
-
 	// kernel size
-	// radius := 3
+	radius := 1
 	point := img.Bounds().Size()
 	width := point.X
 	height := point.Y
-	var constant uint16 = 255
+	var constant uint16 = 0
 
 	fmt.Println(fmt.Sprintf("Width : %d, Height : %d", width, height))
 	// Iterate every pixel
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			topLeftX := max(0, i-radius)
+			topLeftY := max(0, j-radius)
+			botRightX := min(width-1, i+radius)
+			botRightY := min(height-1, j+radius)
+
+			// Starts with 1 as this starts off with the values at i, j
+			var amount int = 1
 			col := img.At(i, j)
 			r, g, b, _ := col.RGBA()
-			r8 := clamp(uint16(r/256) + constant)
-			g8 := clamp(uint16(g/256) + constant)
-			b8 := clamp(uint16(b/256) + constant)
+			for y := topLeftY; y <= botRightY; y++ {
+				for x := topLeftX; x <= botRightX; x++ {
+					if x == i && y == j {
+						continue
+					}
+					// The values of the ones at the surrounding pixels within the kernel
+					sCol := img.At(x, y)
+					sR, sG, sB, _ := sCol.RGBA()
+					r += sR
+					g += sG
+					b += sB
+					amount++
+				}
+			}
+			// fmt.Println("Amount : ", amount)
+
+			r8 := clampRGB(uint16(r/uint32(amount)/256) + constant)
+			g8 := clampRGB(uint16(g/uint32(amount)/256) + constant)
+			b8 := clampRGB(uint16(b/uint32(amount)/256) + constant)
 			mod.Set(i, j, color.RGBA{
 				R: r8,
 				G: g8,
